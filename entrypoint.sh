fe3d50f51a586219b62bdbf72502301ef5400714
andreibyf <andreibyf@yahoo.com>
2025-06-06 01:10:06 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,30 +5,30 @@ SITE_NAME=${SITE_NAME:-"site.local"}
 DB_ROOT_PASSWORD=${DB_ROOT_PASSWORD:-"root"}
 ADMIN_PASSWORD=${ADMIN_PASSWORD:-"admin"}
 
-# Install jq (for clean JSON patching)
-apt update && apt install -y jq
-
-# Create site if not exists
+# Create site only if it doesn't exist
 if [ ! -d "sites/$SITE_NAME" ]; then
   echo "Creating site: $SITE_NAME"
 
   bench new-site "$SITE_NAME" \
     --no-mariadb-socket \
     --mariadb-root-password="$DB_ROOT_PASSWORD" \
-    --admin-password="$ADMIN_PASSWORD" \
-    --force
+    --admin-password="$ADMIN_PASSWORD"
 
   echo "Installing frappe_crm"
   bench get-app https://github.com/frappe/frappe_crm --branch version-14
   bench --site "$SITE_NAME" install-app frappe_crm
+
+  # Patch common_site_config.json right after new-site
+  python3 -c "
+import json
+f = 'sites/common_site_config.json'
+with open(f) as file:
+    d = json.load(file)
+d['webserver_port'] = 8000
+with open(f, 'w') as file:
+    json.dump(d, file)
+"
 fi
 
-# Patch common_site_config.json safely
-if [ -f sites/common_site_config.json ]; then
-  jq 'if has("webserver_port") then . else . + { "webserver_port": 8000 } end' \
-    sites/common_site_config.json > sites/tmp.json && \
-    mv sites/tmp.json sites/common_site_config.json
-fi
-
-# Start Frappe using Gunicorn
+# Start Frappe
 exec bench start
